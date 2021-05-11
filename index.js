3b9908f38399aa60f9137b322f1ed9f5c6f0eae4
Hajar Mrabet <hajarmr98@gmail.com>
2021-05-11 16:02:33 +0200

    Minumum

---
+++ index.js
@@ -0,0 +1,21 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+const nodemailer = require("nodemailer");
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express());
+app.use(cors());
+const port = process.env.port || 1111;
+
+
+app.get('/', (req, res) => {
+	let chiste1="Nhoob.com -> Wipergaming.com"
+	res.send(chiste1);
+});
+
+app.listen(port, () => console.log(`Escuchando por el puerto ${port} :)`));
